972d10b340107b5f7e44c3dd909f07a383e6a340
takahirom <takam.dev@gmail.com>
2015-08-20 23:30:02 +0900

    Add detail comment

--- app/src/debug/java/com/example/takahirom/sandboxapp/DebugApplication.java
+++ app/src/debug/java/com/example/takahirom/sandboxapp/DebugApplication.java
@@ -2,6 +2,7 @@ package com.example.takahirom.sandboxapp;
 
 import android.os.Handler;
 import android.os.StrictMode;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.facebook.stetho.Stetho;
@@ -11,14 +12,17 @@ import com.squareup.leakcanary.LeakCanary;
  * Created by takahirom on 15/08/15.
  */
 public class DebugApplication extends MyApplication {
+
     @Override
     public void onCreate() {
         super.onCreate();
         Toast.makeText(this, "this is debug", Toast.LENGTH_SHORT).show();
+        Log.d("DebugApplication", "onCreate");
         new Handler().postAtFrontOfQueue(
                 new Runnable() {
                     @Override
                     public void run() {
+                        // workaround https://code.google.com/p/android/issues/detail?id=35298
                         StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
                                         .detectDiskReads()
                                         .detectDiskWrites()
